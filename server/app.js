7c948dfb5628336bad2c740e51b879a7f6814a14
Van Nguyen <nguyenvantk21@gmail.com>
2024-02-14 11:32:29 +0700

    update method get

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const port = 3000;
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
 
@@ -19,8 +21,7 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    const responseData = { message: 'Server ready!' };
-    res.json(responseData);
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 app.use('/api', authRoutes);
 
